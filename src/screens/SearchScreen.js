2869e53c4a2ab52f1e423a321b84b67812b90470
Varun Raj <varunraj82@gmail.com>
2020-11-18 23:58:42 -0500

    refactor hook logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import {View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp';
 import useResults from '../hooks/useResults' // contains useState and useEffect logic
 
 const SearchScreen = () =>{
